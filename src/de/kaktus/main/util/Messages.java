7430cdd59c29c03b28197c27dc825ef1b5aa0035
KeinKaktus <149959502+KeinKaktus@users.noreply.github.com>
2024-03-26 23:49:18 +0100

    Update Messages.java

--- src/de/kaktus/main/util/Messages.java
+++ src/de/kaktus/main/util/Messages.java
@@ -7,5 +7,6 @@ public class Messages {
     public static String deactivate = "&cMaintenance deactivated!";
     public static String motd = "&cYourServer.de is in Maintenance! Edit in config.yml";
     public static String kickMsg = "&cWe are currently in Maintenance!";
+    public static String defaultMOTD = "&aDefault MOTD edit in config.yml";
     public static boolean isActive;
 }
